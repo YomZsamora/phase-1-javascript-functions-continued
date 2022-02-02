ba6169d410ad1734e394e683be3ade6290e9972e
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 17:53:37 +0300

    Defined mondayWork function expression that uses the default activity 'go to the office' when no arguments are passed.

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function saturdayFun(defaultActivity = 'roller-skate'){
 	return `This Saturday, I want to ${defaultActivity}!`;
 }
+
+// Defined mondayWork function expression that uses the default activity 'go to the office' when no arguments are passed.
+let mondayWork = function(defaultActivity = 'go to the office') {
+	return `This Monday, I will ${defaultActivity}.`
+}
