4a8a11d5e30f3cdd00e538a4a8a8f2633431ea68
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 17:57:20 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Uses the default activity 'roller-skate' when no arguments are passed
 function saturdayFun(defaultActivity = 'roller-skate'){
 	return `This Saturday, I want to ${defaultActivity}!`;
