ec6cbf0226510735b39a88dbbaa7668de1c251ea
yomZsamora <samora.y@adzumi.co.ke>
2022-02-02 17:54:36 +0300

    Defined wrapAdjective function according to the specification that when initialized with '*' creates a function that, when called, wraps an adjective in a highlight.

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function saturdayFun(defaultActivity = 'roller-skate'){
 let mondayWork = function(defaultActivity = 'go to the office') {
 	return `This Monday, I will ${defaultActivity}.`
 }
+
+// Defines wrapAdjective function according to the specification that when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+let wrapAdjective = function(visualFlairString = '*') {
+	return function(adjective = 'special') {
+		return `You are ${visualFlairString}${adjective}${visualFlairString}!`;
+	}
+}
